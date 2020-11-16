686e309f20cec084615bf772b11180a75a8c7f3c
Denis <minldft@gmail.com>
2020-11-16 22:24:58 +0300

    H!

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,5 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        vue-test
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <h1>HI!!!!!!!!!!!!!</h1>
 </template>
 
 <script>
